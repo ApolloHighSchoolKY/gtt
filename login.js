a6bbf9951c41c929d9381edf65590d4c2d557047
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2020-11-30 13:03:50 -0600

    Create login.js
    Fix Authorization Later

---
+++ login.js
@@ -0,0 +1,12 @@
+<!--//
+function pasuser(form) {
+if (form.id.value=="JavaScript") { 
+if (form.pass.value=="Kit") {              
+location="page2.html" 
+} else {
+alert("Invalid Password")
+}
+} else {  alert("Invalid UserID")
+}
+}
+//-->

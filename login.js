c29d535c551d48bcc5926245871752023405a86c
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2020-12-01 13:51:00 -0600

    Formating

--- login.js
+++ login.js
@@ -1,12 +1,20 @@
 <!--//
-function pasuser(form) {
-if (form.username.value=="JavaScript") { 
-if (form.password.value=="Kit") {              
-location= "index.html"; 
-} else {
-alert("Invalid Password")
-}
-} else {  alert("Invalid UserID")
-}
+function pasuser(form) 
+{
+  if (form.username.value=="JavaScript")
+  { 
+      if (form.password.value=="Kit")
+      {              
+      location= "index.html" 
+      }
+      else 
+      {
+      alert("Invalid Password")
+      }
+  else 
+  { 
+  alert("Invalid UserID")
+  }
+  }
 }
 //-->

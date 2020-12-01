062cdd3a6b5499212cc5a4091637a7f0ea9ed999
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2020-12-01 12:23:39 -0600

    Corrected ID Names
    Names of the ID were not matching the ones on login.html

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 <!--//
 function pasuser(form) {
-if (form.id.value=="JavaScript") { 
-if (form.pass.value=="Kit") {              
+if (form.username.value=="JavaScript") { 
+if (form.password.value=="Kit") {              
 location="index.html" 
 } else {
 alert("Invalid Password")
